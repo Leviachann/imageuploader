07de1ed2c5102e6d758c949f33d26f11cf4971f4
ScaraMouche <114559016+Leviachann@users.noreply.github.com>
2023-08-09 16:02:58 +0400

    Add files via upload

---
+++ ImageSender/ImageSender/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls; 
+using System.Windows.Media.Imaging;
+
+
+namespace ImageGalleryServer
+{
+    public partial class MainWindow : Window
+    {
+        private List<byte[]> imageBytesList = new List<byte[]>();
+        private Thread serverThread;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            serverThread = new Thread(StartServer);
+            serverThread.Start();
+        }
+
+        private void StartServer()
+        {
+            try
+            {
+                TcpListener listener = new TcpListener(IPAddress.Any, 12345);
+                listener.Start();
+
+                while (true)
+                {
+                    using (TcpClient client = listener.AcceptTcpClient())
+                    {
+                        byte[] buffer = new byte[1024];
+                        using (NetworkStream stream = client.GetStream())
+                        {
+                            int bytesRead;
+                            using (var ms = new System.IO.MemoryStream())
+                            {
+                                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                                {
+                                    ms.Write(buffer, 0, bytesRead);
+                                }
+                                imageBytesList.Add(ms.ToArray());
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Server Error: " + ex.Message);
+            }
+        }
+
+        private void UpdateImageGallery()
+        {
+            GalleryStackPanel.Children.Clear();
+            foreach (byte[] imageBytes in imageBytesList)
+            {
+                BitmapImage bitmap = new BitmapImage();
+                using (var stream = new System.IO.MemoryStream(imageBytes))
+                {
+                    bitmap.BeginInit();
+                    bitmap.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmap.StreamSource = stream;
+                    bitmap.EndInit();
+                }
+                System.Windows.Controls.Image imageControl = new System.Windows.Controls.Image();
+                imageControl.Source = bitmap;
+                imageControl.Width = 150;
+                imageControl.Height = 150;
+                GalleryStackPanel.Children.Add(imageControl);
+            }
+        }
+
+        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateImageGallery();
+        }
+    }
+}
---
+++ ImageSenderClient/ImageSenderClient/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace ImageGalleryClient
+{
+    public partial class MainWindow : Window
+    {
+        private const string ServerIPAddress = "127.0.0.1";
+        private const int ServerPort = 12345;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UploadButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.png;*.jpg;*.jpeg;*.gif;*.bmp)|*.png;*.jpg;*.jpeg;*.gif;*.bmp";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    TcpClient client = new TcpClient(ServerIPAddress, ServerPort);
+                    using (NetworkStream stream = client.GetStream())
+                    using (FileStream fileStream = new FileStream(openFileDialog.FileName, FileMode.Open, FileAccess.Read))
+                    {
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+                        while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            stream.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                    MessageBox.Show("Image uploaded successfully.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error uploading image: " + ex.Message);
+                }
+            }
+        }
+    }
+}
